cbbf32eb555ddd71f1a36966a2ddffbc1cf8e792
Josselin <julien.josselin75@hotmail.fr>
2021-06-14 12:12:36 +0200

    player: implement hurt animation + health/damage code

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,21 @@
 extends KinematicBody2D
 
+signal health_updated(health)
+signal killed()
+
 const UP = Vector2(0, -1)
 const GRAVITY = 120
 const SPEED = 350
 const JUMP_HEIGHT = -150
 var motion = Vector2()  # can store x and y value
 var state_machine
+
+export (float) var max_health = 100
+
+# onready bcs we don't know export val at start
+onready var health = max_health setget _set_health
+onready var invulnerability_timer = $InvulnerabilityTimer
+onready var effects_animation = $Cart/AnimationPlayer
 
 # func hurt()
 	# travel() finds the shortest path btw current node and "hurt" node
@@ -36,3 +46,25 @@ func _physics_process(_delta):
 	get_input()
 	motion = move_and_slide(motion, UP)  # UP to show which direction is up
 	pass
+
+func damage(amount):
+	if invulnerability_timer.is_stopped():
+		invulnerability_timer.start()
+		_set_health(health - amount)
+		effects_animation.play("damage")
+
+func kill():
+	pass
+
+func _set_health(value):
+	var prev_health = health
+	# health should not be <0 or >max_health (so use clamp)
+	health = clamp(value, 0, max_health)
+	if health != prev_health:
+		emit_signal("health_updated", health)
+		if health == 0:
+			kill()
+			emit_signal("killed")
+
+func _on_InvulnerabilityTimer_timeout():
+	effects_animation.play("idle")
