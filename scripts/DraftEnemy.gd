0be1ec646fb71164ef55c2209e280f807ea6003e
Josselin <julien.josselin75@hotmail.fr>
2021-06-27 12:19:55 +0200

    enemy: dr aftenemy detects player

--- scripts/DraftEnemy.gd
+++ scripts/DraftEnemy.gd
@@ -13,6 +13,8 @@ func _ready():
 	pass
 
 func _process(_delta):
+	# if $AnimationPlayer.current_animation == "attack": # stop walking
+	# 	return
 	move_character()
 
 func move_character():
@@ -23,8 +25,17 @@ func move_character():
 
 func hit():
 	$PlayerDetector.monitoring = true
+	print("hit")
 
 func end_of_hit():
 	$PlayerDetector.monitoring = false
 # func start_walk():
 # 	$AnimationPlayer.play("move")
+
+func _on_PlayerDetector_body_entered(body):
+	# $AnimationPlayer.play("attack")
+	hit()
+	pass
+
+func _on_AttackDetector_body_entered(body):
+	get_tree().reload_current_scene()
