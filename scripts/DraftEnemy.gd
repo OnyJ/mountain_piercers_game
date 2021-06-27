28419bc7b7ef32753e383706a12df48cbf0887af
Josselin <julien.josselin75@hotmail.fr>
2021-06-27 11:26:08 +0200

    enemy: create DraftEnemy scene and script - he walks left

---
+++ scripts/DraftEnemy.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+# checkpoint:
+# https://www.youtube.com/watch?v=4WywpSBncFI
+
+var is_moving_left = true
+var gravity = 10
+var velocity = Vector2(0, 0)
+var speed = 330
+
+func _ready():
+	# $AnimationPlayer.play("walk")
+	pass
+
+func _process(__delta):
+	move_character()
+
+func move_character():
+	if is_moving_left:
+		velocity.x = -speed
+	velocity.y += gravity
+	velocity = move_and_slide(velocity, Vector2.UP )
+
+func hit():
+	$PlayerDetector.monitoring = false
+
+# func start_walk():
+# 	$AnimationPlayer.play("move")
